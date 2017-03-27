06950a8f7584e463e7f617361a501e11011b2f99
Hendrik-Jan de Harder <hendrik-jan@mediamonks.com>
2017-03-27 23:02:46 +0200

    add build preview server

---
+++ build/preview-server.js
@@ -0,0 +1,29 @@
+const config = require('../config');
+if (!process.env.NODE_ENV) process.env.NODE_ENV = JSON.parse(config.dev.env.NODE_ENV);
+const path = require('path');
+const express = require('express');
+const webpack = require('webpack');
+const opn = require('opn');
+
+// default port where dev server listens for incoming traffic
+const port = 4040;
+
+const app = express();
+
+// handle fallback for HTML5 history API
+app.use(require('connect-history-api-fallback')());
+
+app.use(express.static(path.join(__dirname, '../dist')));
+
+const uri = 'http://localhost:' + port;
+
+console.log('> Listening at ' + uri + '\n');
+
+module.exports = app.listen(port, function (err) {
+	if (err) {
+		console.log(err);
+		return;
+	}
+
+	opn(uri);
+});
