d751660260b09e4e83e130038dd397ca37d7a9f8
Hendrik-Jan de Harder <hendrik-jan@mediamonks.com>
2017-03-28 22:52:48 +0200

    add gzip to preview server

--- build/preview-server.js
+++ build/preview-server.js
@@ -4,6 +4,7 @@ const path = require('path');
 const express = require('express');
 const webpack = require('webpack');
 const opn = require('opn');
+const compression = require('compression');
 
 // default port where dev server listens for incoming traffic
 const port = 4040;
@@ -12,6 +13,7 @@ const app = express();
 
 // handle fallback for HTML5 history API
 app.use(require('connect-history-api-fallback')());
+app.use(compression());
 
 app.use(express.static(path.join(__dirname, '../dist')));
 
