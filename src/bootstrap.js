e6f7ce7017649446a7370c0a71ae0e1cf4e4e8a1
Jesse Linthorst <jesse@mediamonks.com>
2017-04-04 14:06:42 +0200

    Move app required startUp logic back to bootstrap
    Added a initPlugins method in startUp where plugins can be initialized.
    Removed the 18n-manager seperated file.

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -3,12 +3,16 @@ import 'modernizr';
 import 'polyfill';
 
 import Vue from 'vue';
+import App from 'App';
 import filter from 'filter';
 import directive from 'directive';
 import component from 'component';
 import router from 'router';
 import store from 'store';
 import startUp from 'control/startUp';
+import localeConfig from 'config/localeConfig';
+import VueI18nManager from 'vue-i18n-manager';
+import { sync } from 'vuex-router-sync';
 
 // register filters globally
 Object.keys(filter).forEach(key => Vue.filter(key, filter[key]));
@@ -19,5 +23,26 @@ Object.keys(directive).forEach(key => Vue.directive(key, directive[key]));
 // register components globally
 Object.keys(component).forEach(key => Vue.component(key, component[key]));
 
+if (localeConfig.localeEnabled) {
+	Vue.use(VueI18nManager, {
+		store,
+		router,
+		config: localeConfig.config,
+		proxy: localeConfig.proxy,
+	});
+
+	Vue.initI18nManager();
+}
+
+// sync router data to store
+sync(store, router);
+
+// Init new vue app
+const app = new Vue({
+	...App,
+	router,
+	store,
+});
+
 // Mount the app after startUp
-startUp(router, store).then(app => app.$mount('#app'));
+startUp().then(() => app.$mount('#app'));
--- src/control/startUp.js
+++ src/control/startUp.js
@@ -1,17 +1,19 @@
 import Vue from 'vue';
-import App from 'App';
-import { sync } from 'vuex-router-sync';
 import ConfigPlugin from 'config/ConfigPlugin';
-import i18nManagerInit from './18n-manager';
 
-const startUp = (router, store) => (
-	Promise.all([
-		i18nManagerInit(router, store),
-		// Reference the configManager using vm.$config
-		Vue.use(ConfigPlugin),
-		// sync router data to store
-		sync(store, router),
-	])
-	.then(() => new Vue({ ...App, router, store })));
+const initPlugins = () => {
+	// Plugin to reference the configManager using vm.$config
+	Vue.use(ConfigPlugin);
+};
+
+const startUp = () => {
+	// Initialise plugins
+	initPlugins();
+
+	// Add async methods to the Promise.all array
+	return Promise.all([
+		Promise.resolve(),
+	]);
+};
 
 export default startUp;
