eaf590bf5fc25c235bdc1cc84132153c714bf46b
Hendrik-Jan de Harder <hendrik-jan@mediamonks.com>
2017-04-05 23:50:59 +0200

    implement pagename in routes

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,9 +1,11 @@
 import HomePage from 'page/HomePage';
 import Pages from 'data/enum/Pages';
+import PageNames from 'data/enum/PageNames';
 
 export default [
 	{
 		path: Pages.HOME,
 		component: HomePage,
+		name: PageNames.HOME,
 	},
 ];
