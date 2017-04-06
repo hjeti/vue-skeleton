ff68ab6ac6c96ddad7b34386ac4e61690b3f429f
Jesse Linthorst <jesse@mediamonks.com>
2017-04-06 13:58:21 +0200

    Reformat config/index.js

--- config/index.js
+++ config/index.js
@@ -4,29 +4,29 @@ const versionPath = 'version/' + new Date().getTime() + '/';
 const publicPath = '/';  //change if site is running in a subfolder on the server
 
 module.exports = {
-    build: {
-        env: {
+	build: {
+		env: {
 			NODE_ENV: JSON.stringify('production'),
 			VERSIONED_STATIC_ROOT: JSON.stringify(publicPath + versionPath + 'static/'),
 			STATIC_ROOT: JSON.stringify(publicPath + 'static/')
 		},
-        index: path.resolve(__dirname, '../dist/index.html'),
-	    versionPath: versionPath,
+		index: path.resolve(__dirname, '../dist/index.html'),
+		versionPath: versionPath,
 		publicPath: publicPath,
 		enableESLintLoader: true,
-	    enableTSLintLoader: true,
-    },
-    dev: {
-        env: {
+		enableTSLintLoader: true,
+	},
+	dev: {
+		env: {
 			NODE_ENV: JSON.stringify('development'),
 			VERSIONED_STATIC_ROOT: JSON.stringify('/static/'),
 			STATIC_ROOT: JSON.stringify('/static/')
 		},
-        port: 8080,
-        proxyTable: {},
+		port: 8080,
+		proxyTable: {},
 		enableESLintLoader: true,
-	    enableTSLintLoader: false,
-    },
+		enableTSLintLoader: false,
+	},
 	useHttps: false,
 	prePush: ['esLintCheck', 'tsLintCheck'],
 };
