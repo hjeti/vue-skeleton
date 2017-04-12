844ca62ec29e74f3fad0d99ae79a8dd18b614425
Hendrik-Jan de Harder <hendrik-jan@mediamonks.com>
2017-04-12 13:36:49 +0200

    predefine namespace and mutationtypes in store templates

--- src/store/module/user/index.js
+++ src/store/module/user/index.js
@@ -1,8 +1,10 @@
 import user, { SET_FIRST_NAME, SET_LAST_NAME } from './user';
 
+export const UserNamespace = 'user';
+
 export const UserMutationTypes = {
-	SET_FIRST_NAME: `user/${SET_FIRST_NAME}`,
-	SET_LAST_NAME: `user/${SET_LAST_NAME}`,
+	SET_FIRST_NAME: `${UserNamespace}/${SET_FIRST_NAME}`,
+	SET_LAST_NAME: `${UserNamespace}/${SET_LAST_NAME}`,
 };
 
 export default user;
--- template/complex-store/{name_cc}/index.js
+++ template/complex-store/{name_cc}/index.js
@@ -1,3 +1,8 @@
 import {{name_cc}} from './{{name_cc}}';
 
+export const {{name_pc}}Namespace = '{{name_cc}}';
+
+export const {{name_pc}}MutationTypes = {
+};
+
 export default {{name_cc}};
