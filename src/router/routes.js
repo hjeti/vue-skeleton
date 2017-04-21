bc04706b9dd079fa71ed89b676f39cd852b7d529
Hendrik-Jan de Harder <hendrik-jan@mediamonks.com>
2017-04-21 16:22:58 +0200

    add documentation about code splitting

--- src/router/routes.js
+++ src/router/routes.js
@@ -2,12 +2,6 @@ import HomePage from 'page/HomePage';
 import Pages from 'data/enum/Pages';
 import PageNames from 'data/enum/PageNames';
 
-// Code splitting example
-// const HomePage = resolve =>
-// 	require.ensure([], (require) => {
-// 		resolve(require('page/HomePage').default);
-// 	}, 'HomePage');
-
 export default [
 	{
 		path: Pages.HOME,
