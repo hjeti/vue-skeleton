77d18827de79cb30282cd8b46e1a7f29bf603afd
Hendrik-Jan de Harder <hendrik-jan@mediamonks.com>
2017-07-10 14:09:46 +0200

    remove connected page and component template. Will be replaced by variable connected in page and component template.

--- template/connected-component/{name_pc}/index.js
+++
@@ -1,9 +0,0 @@
-import { connect } from 'vuex-connect';
-import {{name_pc}} from './{{name_pc}}';
-
-export default connect({
-	stateToProps: {
-	},
-	mutationsToProps: {
-	},
-})({{name_pc}}.name, {{name_pc}});
