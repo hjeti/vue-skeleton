67a8075516c4aae6537ee895206de55a54764fc2
Jesse Linthorst <jesse@mediamonks.com>
2017-07-29 17:41:15 +0200

    Update Icon Component to use require.context for loading icons

--- src/component/Icon/Icon.js
+++ src/component/Icon/Icon.js
@@ -1,17 +1,34 @@
-import icons from 'asset/svg';
-
 export default {
 	name: 'Icon',
 	props: {
-		name: {
-			validator(value) {
-				return !!icons[value];
-			},
+		name: String,
+	},
+
+	computed: {
+		fileName() {
+			return `./${this.name}.svg`;
 		},
 	},
-	computed: {
-		icon() {
-			return icons[this.name];
-		},
+
+	data: () => ({
+		icon: null,
+	}),
+
+	beforeCreate() {
+		// As we cannot generate this string on compile time you can use the following gist:
+		// https://gist.github.com/jesse-mm/34a73df1390ec6715055af08fe44e630
+
+		this.requireContext = require.context(
+			// eslint-disable-next-line max-len
+			'!!bundle-loader?{"lazy":true,"name":"app"}!svg-inline-loader!svgo-loader?{"plugins":[{"removeStyleElement":true},{"removeComments":true},{"removeDesc":true},{"removeUselessDefs":true},{"removeTitle":true},{"removeMetadata":true},{"removeComments":true},{"cleanupIDs":{"remove":true,"prefix":""}},{"convertColors":{"shorthex":false}}]!asset/svg',
+			true,
+			/\.svg$/,
+		);
+	},
+
+	mounted() {
+		this.requireContext(this.fileName)((svgData) => {
+			this.icon = svgData;
+		});
 	},
 };
