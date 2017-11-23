4cc7181d5e0d334a05ea64f3af0254be351a04f5
Hendrik-Jan de Harder <hendrik-jan@mediamonks.com>
2017-11-23 13:13:18 +0100

    reformat files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,44 +1,44 @@
 // http://eslint.org/docs/user-guide/configuring
 
 module.exports = {
-	root: true,
-	parser: 'babel-eslint',
-	parserOptions: {
-		sourceType: 'module',
-	},
-	env: {
-		browser: true,
-	},
-	extends: ['airbnb-base', 'prettier'],
-	plugins: ['html', 'import', 'prettier'],
-	settings: {
-		'import/resolver': {
-			webpack: {
-				config: 'build/webpack.base.conf.js',
-			},
-		},
-	},
-	rules: {
-		// don't require .vue extension when importing
-		'import/extensions': [
-			'error',
-			'always',
-			{
-				js: 'never',
-				vue: 'never',
-				ts: 'never',
-			},
-		],
-		// allow debugger during development
-		'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
-		'no-console': process.env.NODE_ENV === 'production' ? 1 : 0,
-		'max-len': ['error', 100],
-		'no-param-reassign': 0,
+  root: true,
+  parser: 'babel-eslint',
+  parserOptions: {
+    sourceType: 'module',
+  },
+  env: {
+    browser: true,
+  },
+  extends: ['airbnb-base', 'prettier'],
+  plugins: ['html', 'import', 'prettier'],
+  settings: {
+    'import/resolver': {
+      webpack: {
+        config: 'build/webpack.base.conf.js',
+      },
+    },
+  },
+  rules: {
+    // don't require .vue extension when importing
+    'import/extensions': [
+      'error',
+      'always',
+      {
+        js: 'never',
+        vue: 'never',
+        ts: 'never',
+      },
+    ],
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    'no-console': process.env.NODE_ENV === 'production' ? 1 : 0,
+    'max-len': ['error', 100],
+    'no-param-reassign': 0,
     // only for use with getter-setters
     'no-underscore-dangle': 0,
-		'prettier/prettier': [
-			'error',
-			{ singleQuote: true, trailingComma: 'all', printWidth: 100, tabWidth: 2 },
-		],
-	},
+    'prettier/prettier': [
+      'error',
+      { singleQuote: true, trailingComma: 'all', printWidth: 100, tabWidth: 2 },
+    ],
+  },
 };
--- .tslintrc.js
+++ .tslintrc.js
@@ -1,14 +1,11 @@
 module.exports = {
-	extends: [
-		'tslint-config-airbnb',
-		'tslint-config-prettier',
-	],
-	defaultSeverity: 'error',
-	rules: {
-		'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
-		'no-console': process.env.NODE_ENV === 'production' ? 1 : 0,
-		'prefer-array-literal': [ true, { 'allow-type-parameters': true } ],
-		'variable-name': [ true, 'allow-pascal-case' ],
-		'max-line-length': [true, 100],
-	},
+  extends: ['tslint-config-airbnb', 'tslint-config-prettier'],
+  defaultSeverity: 'error',
+  rules: {
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    'no-console': process.env.NODE_ENV === 'production' ? 1 : 0,
+    'prefer-array-literal': [true, { 'allow-type-parameters': true }],
+    'variable-name': [true, 'allow-pascal-case'],
+    'max-line-length': [true, 100],
+  },
 };
