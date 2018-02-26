4ba78b3503e3807f595c8b986cd266dac171cce2
Hendrik-Jan de Harder <hendrikjan.deharder@gmail.com>
2018-02-26 17:13:23 -0300

    add warning close #38

--- src/component/index.js
+++ src/component/index.js
@@ -1,6 +1,9 @@
 import Icon from './Icon';
 
-// add components you want to register globally
+// add components you want to register globally.
+// Warning: only use this for small components without much logic as registering a component
+// globally can create circular dependencies in some cases. For example when you use the store
+// in your component
 export default {
   Icon,
 };
