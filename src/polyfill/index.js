ac08fe31c8273aa0347efd41626acbed8bbc35e1
Hendrik-Jan de Harder <hendrikjan.deharder@gmail.com>
2018-02-26 16:43:03 -0300

    add stage-3 and stage-4 polyfills

--- src/polyfill/index.js
+++ src/polyfill/index.js
@@ -1,5 +1,9 @@
+// will be changed during build-time by babel-preset-env from webpack config
 import 'babel-polyfill';
-import 'core-js/es7/array';
-import 'classlist-polyfill';
+// not included by the above, review here: https://github.com/zloirock/core-js#stage-4-proposals
+// eslint-disable-next-line import/no-extraneous-dependencies
+import 'core-js/stage/4';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import 'core-js/stage/3';
 
 export default {};
