39f2321a60fe7213e0ae4dec22ecbeccef39bde9
Hendrik-Jan de Harder <hendrikjan.deharder@gmail.com>
2018-06-25 12:08:48 -0300

    fix lint-staged commands so only staged files are linted.
    close #63

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,18 +2,22 @@ const config = require('./build-tools/config');
 
 const defaultSettings = ['prettier --write', 'git add'];
 
+const jsLintCommand = 'eslint --ext .js,.vue --cache';
+const tsLintCommand = 'tslint -p tsconfig.json';
+const scssLintCommand = 'stylelint --cache';
+
 const jsSettings = config.lintStaged.eslintEnabled
-  ? [...defaultSettings, 'npm run lint:js']
+  ? [...defaultSettings, jsLintCommand]
   : [...defaultSettings];
 
-const vueSettings = config.lintStaged.eslintEnabled ? ['npm run lint:js'] : [];
+const vueSettings = config.lintStaged.eslintEnabled ? [jsLintCommand] : [];
 
 const tsSettings = config.lintStaged.tslintEnabled
-  ? [...defaultSettings, 'npm run lint:ts']
+  ? [...defaultSettings, tsLintCommand]
   : [...defaultSettings];
 
 const scssSettings = config.lintStaged.stylelintEnabled
-  ? [...defaultSettings, 'npm run lint:scss']
+  ? [...defaultSettings, scssLintCommand]
   : [...defaultSettings];
 
 module.exports = {
