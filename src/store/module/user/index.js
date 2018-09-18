4037d1362b68ced30d336281b4f7044402955255
Hendrik-Jan de Harder <hendrikjan.deharder@gmail.com>
2018-09-18 16:50:04 +0200

    implement new store setup to make it easier to work with the store
    see #36
    see #67

--- src/store/module/user/index.js
+++ src/store/module/user/index.js
@@ -1,10 +1,3 @@
-import user, { SET_FIRST_NAME, SET_LAST_NAME } from './user';
-
-export const UserNamespace = 'user';
-
-export const UserMutationTypes = {
-  SET_FIRST_NAME: `${UserNamespace}/${SET_FIRST_NAME}`,
-  SET_LAST_NAME: `${UserNamespace}/${SET_LAST_NAME}`,
-};
+import user from './user';
 
 export default user;
